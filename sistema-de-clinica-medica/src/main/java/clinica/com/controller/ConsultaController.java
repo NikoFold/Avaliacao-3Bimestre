0350dfa3f177ab016d2e37556089f7c95a8ef2dd
NikoFold <enrimelo18@gmail.com>
2025-10-14 14:35:25 -0300

    Add files via upload

---
+++ sistema-de-clinica-medica/src/main/java/clinica/com/controller/ConsultaController.java
@@ -0,0 +1,73 @@
+package clinica.com.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import clinica.com.entity.Consulta;
+import clinica.com.service.ConsultaService;
+import clinica.com.service.PacienteService;
+
+@Controller
+@RequestMapping("/consulta")
+public class ConsultaController {
+
+    @Autowired
+    private ConsultaService consultaService;
+    
+    @Autowired
+    private PacienteService pacienteService;
+
+    @GetMapping("/form")
+    public String form(Model model) {
+        model.addAttribute("consulta", new Consulta());
+        model.addAttribute("pacientes", pacienteService.list());
+        return "cadastrarConsulta";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute Consulta consulta, @RequestParam("pacienteId") Long pacienteId, Model model) {
+    	if (pacienteId != null) {
+    		consulta.setPaciente(pacienteService.findById(pacienteId));
+    	}
+    	
+        consultaService.save(consulta);
+        model.addAttribute("mensagemSucesso", "Consulta salva com sucesso");
+        model.addAttribute("pacientes", pacienteService.list());
+        return form(model);
+    }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        model.addAttribute("consultas", consultaService.list());
+        return "listarConsulta";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Long id, Model model) {
+        model.addAttribute("consulta", consultaService.findById(id));
+        return "editarConsulta";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute Consulta consulta, Model model) {
+        consultaService.save(consulta);
+        model.addAttribute("mensagemSucesso", "Consulta atualizada com sucesso");
+        model.addAttribute("consultas", consultaService.list());
+        return "listarConsulta";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id, Model model) {
+        consultaService.deleteById(id);
+        model.addAttribute("consultas", consultaService.list());
+        model.addAttribute("mensagemSucesso", "Consulta removida com sucesso");
+        return "listarConsulta";
+    }
+}
---
+++ sistema-de-clinica-medica/src/main/java/clinica/com/controller/PacienteController.java
@@ -0,0 +1,70 @@
+package clinica.com.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import clinica.com.service.PacienteService;
+import clinica.com.entity.Paciente;
+
+@Controller
+@RequestMapping("/paciente")
+public class PacienteController {
+
+    @Autowired
+    private PacienteService pacienteService;
+
+    @GetMapping("/form")
+    public String form(Model model) {
+        model.addAttribute("paciente", new Paciente());
+        return "cadastrarPaciente";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute Paciente paciente, Model model) {
+        try {
+        	pacienteService.save(paciente);
+            model.addAttribute("mensagemSucesso", "Paciente salvo com sucesso");
+        } catch (Exception e) {
+        	model.addAttribute("mensagemErro", e.getMessage());
+        }
+            return form(model);
+    }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        model.addAttribute("pacientes", pacienteService.list());
+        return "listarPaciente";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Long id, Model model) {
+        model.addAttribute("paciente", pacienteService.findById(id));
+        return "editarPaciente";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute Paciente paciente, Model model) {
+        try {
+        	pacienteService.save(paciente);
+        	model.addAttribute("mensagemSucesso", "Paciente atualizado com sucesso");
+        } catch (Exception e) {
+        	model.addAttribute("mensagemErro", e.getMessage());
+        }
+        model.addAttribute("pacientes", pacienteService.list());
+        return "listarPaciente";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id, Model model) {
+        pacienteService.deleteById(id);
+        model.addAttribute("pacientes", pacienteService.list());
+        model.addAttribute("mensagemSucesso", "Paciente removido com sucesso");
+        return "listarPaciente";
+    }
+}
