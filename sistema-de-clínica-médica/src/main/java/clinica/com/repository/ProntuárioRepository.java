b04c24f7947365c6ec088ead9c4655232d878e3e
NikoFold <enrimelo18@gmail.com>
2025-10-14 14:36:00 -0300

    Delete sistema-de-clínica-médica directory

--- sistema-de-clínica-médica/src/main/java/clinica/com/repository/ProntuárioRepository.java
+++
@@ -1,6 +0,0 @@
-package clinica.com.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import clinica.com.entity.Prontuário;
-
-public interface ProntuárioRepository extends JpaRepository<Prontuário, Long> {}
